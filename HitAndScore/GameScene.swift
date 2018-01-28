beb22abde41df7cee9b5711ce47540248c5c997e
Vamshi Krishna <guduru.vkrishna.civ10@itbhu.ac.in>
2018-01-28 19:05:07 +0530

    Init

--- HitAndScore/GameScene.swift
+++ HitAndScore/GameScene.swift
@@ -9,81 +9,199 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var coinMan : SKSpriteNode?
+    var coinTimer : Timer?
+    var bombTimer:Timer?
+    var ceil : SKSpriteNode?
+    var yourScoreLabel : SKLabelNode?
+    var finalScoreLabel : SKLabelNode?
+    var scoreLabel : SKLabelNode?
+    
+    let coinManCategory : UInt32 = 0x1 << 1
+    let coinCategory : UInt32 = 0x1 << 2
+    let bombCategory : UInt32 = 0x1 << 3
+    let groundAndCeilCategory : UInt32 = 0x1 << 4
+    
+    var score = 0
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        physicsWorld.contactDelegate = self
+        
+        coinMan = childNode(withName: "coinMan") as? SKSpriteNode
+        coinMan?.physicsBody?.categoryBitMask = coinManCategory
+        coinMan?.physicsBody?.contactTestBitMask = coinCategory | bombCategory
+        coinMan?.physicsBody?.collisionBitMask = groundAndCeilCategory
+        var coinManRun : [SKTexture] = []
+        for number in 1...5 {
+            coinManRun.append(SKTexture(imageNamed: "frame-\(number)"))
         }
+        coinMan?.run(SKAction.repeatForever(SKAction.animate(with: coinManRun, timePerFrame: 0.09)))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        ceil = childNode(withName: "ceil") as? SKSpriteNode
+        ceil?.physicsBody?.categoryBitMask = groundAndCeilCategory
+        ceil?.physicsBody?.collisionBitMask = coinManCategory
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
+        
+       startTimers()
+        createGrass()
+    }
+    
+    func createGrass() {
+        let sizingGrass = SKSpriteNode(imageNamed: "grass")
+        let numberOfGrass = Int(size.width / sizingGrass.size.width) + 1
+        for number in 0...numberOfGrass {
+            let grass = SKSpriteNode(imageNamed: "grass")
+            grass.physicsBody = SKPhysicsBody(rectangleOf: grass.size)
+            grass.physicsBody?.categoryBitMask = groundAndCeilCategory
+            grass.physicsBody?.collisionBitMask = coinManCategory
+            grass.physicsBody?.affectedByGravity = false
+            grass.physicsBody?.isDynamic = false
+            addChild(grass)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            let grassX = -size.width / 2 + grass.size.width / 2 + grass.size.width * CGFloat(number)
+            grass.position = CGPoint(x: grassX, y: -size.height / 2 + grass.size.height / 2 - 18)
+            let speed = 100.0
+            let firstMoveLeft = SKAction.moveBy(x: -grass.size.width - grass.size.width * CGFloat(number), y: 0, duration: TimeInterval(grass.size.width + grass.size.width * CGFloat(number)) / speed)
+            
+            let resetGrass = SKAction.moveBy(x: size.width + grass.size.width, y: 0, duration: 0)
+            let grassFullMove = SKAction.moveBy(x: -size.width - grass.size.width, y: 0, duration: TimeInterval(size.width + grass.size.width) / speed)
+            let grassMovingForver = SKAction.repeatForever(SKAction.sequence([grassFullMove,resetGrass]))
+            
+            grass.run(SKAction.sequence([firstMoveLeft,resetGrass,grassMovingForver]))
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func startTimers() {
+        coinTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.createCoin()
+        })
+        
+        bombTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
+            self.createBomb()
+        })
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        
+        if scene?.isPaused == false {
+            coinMan?.physicsBody?.applyForce(CGVector(dx: 0, dy: 100_000))
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            let theNodes = nodes(at: location)
+            
+            for node in theNodes {
+                if node.name == "play" {
+                    score = 0
+                    node.removeFromParent()
+                    finalScoreLabel?.removeFromParent()
+                    yourScoreLabel?.removeFromParent()
+                    scene?.isPaused = false
+                    scoreLabel?.text = "Score: \(score)"
+                    startTimers()
+                }
+            }
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func createCoin() {
+        let coin = SKSpriteNode(imageNamed: "coin")
+        coin.physicsBody = SKPhysicsBody(rectangleOf: coin.size)
+        coin.physicsBody?.affectedByGravity = false
+        coin.physicsBody?.categoryBitMask = coinCategory
+        coin.physicsBody?.contactTestBitMask = coinManCategory
+        coin.physicsBody?.collisionBitMask = 0
+        addChild(coin)
+        
+        let maxY = size.height / 2 - coin.size.height / 2
+        let minY = -size.height / 2 + coin.size.height / 2
+        let range = maxY - minY
+        let coinY = maxY - CGFloat(arc4random_uniform(UInt32(range)))
+        
+        coin.position = CGPoint(x: size.width / 2 + coin.size.width / 2, y: coinY)
+        
+        let moveLeft = SKAction.moveBy(x: -size.width - coin.size.width, y: 0, duration: 4)
+        
+        coin.run(SKAction.sequence([moveLeft, SKAction.removeFromParent()]))
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createBomb() {
+        let bomb = SKSpriteNode(imageNamed: "bomb")
+        bomb.physicsBody = SKPhysicsBody(rectangleOf: bomb.size)
+        bomb.physicsBody?.affectedByGravity = false
+        bomb.physicsBody?.categoryBitMask = bombCategory 
+        bomb.physicsBody?.contactTestBitMask = coinManCategory
+        bomb.physicsBody?.collisionBitMask = 0
+        addChild(bomb)
+        
+        let maxY = size.height / 2 - bomb.size.height / 2
+        let minY = -size.height / 2 + bomb.size.height / 2
+        let range = maxY - minY
+        let bombY = maxY - CGFloat(arc4random_uniform(UInt32(range)))
+        
+        bomb.position = CGPoint(x: size.width / 2 + bomb.size.width / 2, y: bombY)
+        
+        let moveLeft = SKAction.moveBy(x: -size.width - bomb.size.width, y: 0, duration: 4)
+        
+        bomb.run(SKAction.sequence([moveLeft, SKAction.removeFromParent()]))
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.categoryBitMask == coinCategory {
+            contact.bodyA.node?.removeFromParent()
+            score += 1
+            scoreLabel?.text = "Score: \(score)"
+        }
+        if contact.bodyB.categoryBitMask == coinCategory {
+            contact.bodyB.node?.removeFromParent()
+            score += 1
+            scoreLabel?.text = "Score: \(score)"
+        }
+        
+        if contact.bodyA.categoryBitMask == bombCategory {
+            contact.bodyA.node?.removeFromParent()
+           gameOver()
+        }
+        
+        if contact.bodyB.categoryBitMask == bombCategory {
+            contact.bodyB.node?.removeFromParent()
+            gameOver()
+        }
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func gameOver() {
+        scene?.isPaused = true
+        
+        coinTimer?.invalidate()
+        bombTimer?.invalidate()
+        
+        yourScoreLabel = SKLabelNode(text: "Your Score:")
+        yourScoreLabel?.position = CGPoint(x: 0, y: 200)
+        yourScoreLabel?.fontSize = 100
+        yourScoreLabel?.zPosition = 1
+        if yourScoreLabel != nil {
+            addChild(yourScoreLabel!)
+        }
+        
+        finalScoreLabel = SKLabelNode(text: "\(score)")
+        finalScoreLabel?.position = CGPoint(x: 0, y: 0)
+        finalScoreLabel?.fontSize = 200
+        finalScoreLabel?.zPosition = 1
+        if finalScoreLabel != nil {
+            addChild(finalScoreLabel!)
+        }
+        
+        let playButton = SKSpriteNode(imageNamed: "play")
+        playButton.position = CGPoint(x: 0, y: -200)
+        playButton.name = "play"
+        playButton.zPosition = 1
+        addChild(playButton)
     }
+    
 }
--- HitAndScore/GameViewController.swift
+++ HitAndScore/GameViewController.swift
@@ -27,8 +27,8 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
